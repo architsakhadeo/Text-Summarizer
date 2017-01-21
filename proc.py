63b51948d1652b6c437030d3816c49b7e27b3966
gemsbound007 <architsakhadeo@gmail.com>
2017-01-21 23:32:46 +0530

    Commented and removed some earlier code

--- proc.py
+++ proc.py
@@ -34,47 +34,9 @@ for i in relations:
 	i[2] = str1.replace(" '","'").decode('utf-8')	
 
 
-	'''
-	if y != '':
-		if y not in f:
-			f[y] = [t.decode('utf-8') for t in v]
-			
-		else:
-			for e in v:	
-				f[y].append(e.decode('utf-8'))
-	'''
 print "------------------------------------------------------------------------------------------------------------------------------------------"
 print relations
-'''
-relations1 = {}
-#removing verbs from third components 
-for i,v in f.items():
-	i = i.decode('utf-8').replace(" '", "'")
-	l = []
-	for p in v:
-		p = p.split()
-		for q in p:
-			if q.lower() in stoplist:
-				p[p.index(q)] = ';'
-		p = ' '.join(p)
-		p = p.split(';')
-		for t in p:
-			if t.strip() != '' and t.strip() not in l:
-				l.append(t.strip())
-	v = l[:]	
-	v = [j.encode('utf-8') for j in v]  
-	relations1[i] = v
 
-
-
-
-newer_sort_list = {}
-
-for i,v in new_sort_list:
-	i = i
-	newer_sort_list[i] = v
-
-'''	
 print "------------------------------------------------------------------"
 print "Relations"
 print "------------------------------------------------------------------"
@@ -89,32 +51,26 @@ for i in relations:
 for i in common_relations:		
 	print i
 
+
+#combining third components if there are substrings
 new = []
 for i in range(len(common_relations)):
 	three = []
 	for j in range(len(common_relations)):
 		if common_relations[i][0] == common_relations[j][0] and common_relations[i][1] == common_relations[j][1]:
 			three.append(common_relations[j][2])
-	print "Three is " + str(three)
 	three1 = three[:]
 	for k in range(len(three)):
 		z = three[k].split(' ')
-		print "Split is " + str(z)
 		for r in range(len(three)):
-			print "Error1"
 			if k != r:
 				count = 0
 				for a in z:
-					print "Error2"
 					if a in three[r]:
-						print "Error3"
 						count += 1
 				if count == len(z):
-					print "Error4"
 					if three[k] in three1:
-						print "Error5"
 						three1.remove(three[k])
-						print "Error6"
 	
 						
 	for q in three1:
@@ -125,32 +81,25 @@ for i in range(len(common_relations)):
 		if sub not in new:	
 			new.append(sub)
 #requires input in decode form
+#combining second components if there are substrings
 newer = []
 for i in range(len(new)):
 	two = []
 	for j in range(len(new)):
 		if new[i][0] == new[j][0] and new[i][2] == new[j][2]:
 			two.append(new[j][1])
-	print "Two is " + str(two)
 	two1 = two[:]
 	for k in range(len(two)):
 		z = two[k].split(' ')
-		print "Split is " + str(z)
 		for r in range(len(two)):
-			print "Error1"
 			if k != r:
 				count = 0
 				for a in z:
-					print "Error2"
 					if a in two[r]:
-						print "Error3"
 						count += 1
 				if count == len(z):
-					print "Error4"
 					if two[k] in two1:
-						print "Error5"
 						two1.remove(two[k])
-						print "Error6"
 	
 						
 	for q in two1:
@@ -168,61 +117,9 @@ print "-------------------------------------------------------------------------
 print "**************************************************************************************************************************************************"
 for i in newer:
 	print i
-	'''for j in range(len(common_relations)):
-		if common_relations[i][0] == common_relations[j][0] and common_relations[i][1] == common_relations[j][1]:
-			counter += 1
-			if i!=j:	
-				if common_relations[i][2] in common_relations[j][2]:
-					count = 0
-					break
-				if common_relations[i][2] not in common_relations[j][2]:
-					count = 1
-	if count == 1:
-		new.append(common_relations[i])
-	if counter == 1:
-		new.append(common_relations[i])
-	if count == 0:
-		continue
-	'''
 	
 
 	
 generate_graphviz_graph(newer,verbose=True)
 os.system('mv /tmp/openie/out.png ./')
-'''
-#common to RAKE and OpenIE
-a = {}
-for i,v in relations1.items():
-	for j,k in newer_sort_list.items():
-		if i in j and i not in a and v != []:
-			a[i] = [t.replace(" '","'") for t in v]
-		if j in i and i not in a and v != []:
-			a[i] = [t.replace(" '","'") for t in v]
-			
-#merging third components belonging to same relation if there are substrings
-for i,v in a.items():
-	p = v[:]
-	for q in range(len(v)):
-		t = v[q].split()
-		for r in range(len(v)):
-			if q != r:
-				count = 0
-				for z in t:
-					if z in v[r]:
-						count += 1
-				if count == len(t):
-					if v[q] in p:
-						p.remove(v[q])
-	a[i] = p
 
-#sorting in decreasing order of number of third components
-z = sorted(a,key=lambda k: len(a[k]),reverse=True)
-for i in z:
-	print i, " -> ",
-	print a[i][0],
-	for k in range(1,len(a[i])):
-		print ',',a[i][k],
-	print
-
-
-'''
